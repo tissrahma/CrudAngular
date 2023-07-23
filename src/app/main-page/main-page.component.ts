2a222dc5228d72320a3c551d397a8105b944c1d4
tissrahma <rahma.tiss@esprit.tn>
2023-07-23 22:33:43 +0100

    third_commit

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+
+interface User {
+  id: number;
+  FirstName: string;
+  LastName: string;
+}
 
 @Component({
   selector: 'app-main-page',
@@ -7,10 +14,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
+  users: User[] = [
+    { id: 1, FirstName: 'John', LastName: 'Doe' },
+    { id: 2, FirstName: 'Jane', LastName: 'Smith' },
+    { id: 3, FirstName: 'Alice', LastName: 'Johnson' },
+    // Add more user data as needed
+  ];
 
-  constructor(private router: Router) { }
+  filteredUsers: User[] = [];
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    // Initialize the filteredUsers with all users initially
+    this.filteredUsers = this.users;
   }
 
   showUsers() {
@@ -19,10 +39,31 @@ export class MainPageComponent implements OnInit {
 
   addUser() {
     this.router.navigate(['add-user']);
-    
   }
 
   updateUsers() {
     // Implement logic to update users (if needed)
+  }
+Search (name:string){
+  
+}
+  onSearch(searchTerm: string) {
+    if (searchTerm.trim() === '') {
+      // If the search input is empty, reset the filteredUsers to show all users
+      this.filteredUsers = this.users;
+    } else {
+     
+      const url = `http://localhost:3000/users/getUserByName/${searchTerm}`;
+      this.http.get<User[]>(url).subscribe(
+        (response) => {
+          console.log(".......",response)
+          this.router.navigate(['details-user/'+response]);
+        },
+        (error) => {
+          console.error('Error while searching:', error);
+          this.filteredUsers = [];
+        }
+      );
+    }
   }
 }
